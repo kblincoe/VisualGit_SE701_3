4e4cc046b9840ea3f2dce7c1e5c6a3dc4fd63c96
Long315 <zhuhualong@yahoo.com>
2018-03-16 00:25:16 +1300

    Revert "Revert "Revert "Merge branch 'master' of https://github.com/kblincoe/VisualGit_SE701_3"""
    This reverts commit 8c3f13fe33820d715e57a99cfe0ba9c3b3cba202.

--- app/misc/router.js
+++ app/misc/router.js
@@ -1,6 +1,4 @@
 var cred;
-var blue = "#39c0ba";
-var gray = "#5b6969";
 function collpaseSignPanel() {
     $('#nav-collapse1').collapse('hide');
 }
@@ -45,47 +43,14 @@ function hideAddRepositoryPanel() {
 function displayDiffPanel() {
     document.getElementById("graph-panel").style.width = "60%";
     document.getElementById("diff-panel").style.width = "40%";
-    displayDiffPanelButtons();
 }
 function hideDiffPanel() {
     document.getElementById("diff-panel").style.width = "0";
     document.getElementById("graph-panel").style.width = "100%";
-    disableDiffPanelEditOnHide();
-    hideDiffPanelButtons();
 }
 function hideAuthenticatePanel() {
     document.getElementById("authenticate").style.zIndex = "-20";
 }
 function displayAuthenticatePanel() {
     document.getElementById("authenticate").style.zIndex = "20";
-}
-function displayDiffPanelButtons() {
-    document.getElementById("save-button").style.visibility = "visible";
-    document.getElementById("cancel-button").style.visibility = "visible";
-}
-function hideDiffPanelButtons() {
-    document.getElementById("save-button").style.visibility = "hidden";
-    document.getElementById("cancel-button").style.visibility = "hidden";
-    disableSaveCancelButton();
-    disableDiffPanelEditOnHide();
-}
-function disableSaveCancelButton() {
-    var saveButton = document.getElementById("save-button");
-    var cancelButton = document.getElementById("cancel-button");
-    saveButton.disabled = true;
-    saveButton.style.backgroundColor = gray;
-    cancelButton.disabled = true;
-    cancelButton.style.backgroundColor = gray;
-}
-function enableSaveCancelButton() {
-    var saveButton = document.getElementById("save-button");
-    var cancelButton = document.getElementById("cancel-button");
-    saveButton.disabled = false;
-    saveButton.style.backgroundColor = blue;
-    cancelButton.disabled = false;
-    cancelButton.style.backgroundColor = blue;
-}
-function disableDiffPanelEditOnHide() {
-    var doc = document.getElementById("diff-panel-body");
-    doc.contentEditable = "false";
 }
