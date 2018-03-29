803af791c9d8e3fedc5eb7ae96f8afa4ce0e4fcc
nathan gavin <nathangavin987@gmail.com>
2018-03-29 21:34:35 +1300

    updated login look to match functionality, can use either email or username to log in

--- app/misc/color.ts
+++ app/misc/color.ts
@@ -52,7 +52,129 @@ function changeColor(color) {
     arp.style.backgroundColor = '#D1D1D1';
     auth.style.backgroundColor = '#D6D6D6';
     before = 'white';
-  } else if (color === 'default') {
+  }
+  else if (color === 'pink') {
+    for (var i = 0; i < head.length; i++) {
+            console.log(head[i]);
+            head[i].className = 'navbar navbar-pink';
+        }
+        for (var i = 0; i < headButton.length; i++) {
+            if (before === 'default') {
+                headButton[i].classList.remove('btn-inverse');
+            }
+            headButton[i].classList.add('btn-default');
+        }
+        for (var i = 0; i < fa.length; i++) {
+            fa[i].setAttribute('style', 'color:white'); 
+        }
+        fp.setAttribute('style', 'background-color: #FFC2C2');
+        for (var i = 0; i < p.length; i++) {
+            p[i].style.color = '#767676'; 
+        }
+        for (var i = 0; i < h1.length; i++) {
+            h1[i].style.color = '#FFA3A3'; 
+        }
+        diffp.style.color = 'white';
+        diffp.style.backgroundColor = 'white';
+        network.style.backgroundColor = '#FFE5E5';
+        footer.style.backgroundColor = '#FFD7D7'; 
+        footer.style.border = '#FFD7D7';
+        arp.style.backgroundColor = '#FFD7D7';
+        auth.style.backgroundColor = '#FFE5E5';
+        before = 'pink';
+    }
+    else if (color === 'blue') {
+      for (var i = 0; i < head.length; i++) {
+              console.log(head[i]);
+              head[i].className = 'navbar navbar-blue';
+          }
+          for (var i = 0; i < headButton.length; i++) {
+              if (before === 'default') {
+                  headButton[i].classList.remove('btn-inverse');
+              }
+              headButton[i].classList.add('btn-default');
+          }
+          for (var i = 0; i < fa.length; i++) {
+              fa[i].setAttribute('style', 'color:white'); 
+          }
+          fp.setAttribute('style', 'background-color: #9DD2FE'); 
+          for (var i = 0; i < p.length; i++) {
+              p[i].style.color = '#767676'; 
+          }
+          for (var i = 0; i < h1.length; i++) {
+              h1[i].style.color = '#4EAFFE'; 
+          }
+          diffp.style.color = 'white'; 
+          diffp.style.backgroundColor = 'white'; 
+          network.style.backgroundColor = '#EEF6FF'; 
+          footer.style.backgroundColor = '#B6DEFF'; 
+          footer.style.border = '#B6DEFF'; 
+          arp.style.backgroundColor = '#DAEEFF'; 
+          auth.style.backgroundColor = '#DAEEFF'; 
+          before = 'blue';
+    }
+    else if (color === 'navy') {
+      for (var i = 0; i < head.length; i++) {
+              console.log(head[i]);
+              head[i].className = 'navbar navbar-navy';
+          }
+          for (var i = 0; i < headButton.length; i++) {
+              if (before === 'default') {
+                  headButton[i].classList.remove('btn-inverse');
+              }
+              headButton[i].classList.add('btn-default');
+          }
+          for (var i = 0; i < fa.length; i++) {
+              fa[i].setAttribute('style', 'color:white');
+          }
+          fp.setAttribute('style', 'background-color: #0066FF'); 
+          for (var i = 0; i < p.length; i++) {
+              p[i].style.color = 'white';
+          }
+          for (var i = 0; i < h1.length; i++) {
+              h1[i].style.color = '#001C83';
+          }
+          diffp.style.color = 'white'; 
+          diffp.style.backgroundColor = 'white';
+          network.style.backgroundColor = '#CCE0FF'; 
+          network.style.border = '#CCE0FF';
+          footer.style.backgroundColor = '#4D94FF'; 
+          footer.style.border = '#4D94FF'; 
+          arp.style.backgroundColor = '#4D94FF'; 
+          auth.style.backgroundColor = '#4D94FF'; 
+          before = 'navy';
+    }
+    else if (color === 'green') {
+      for (var i = 0; i < head.length; i++) {
+              console.log(head[i]);
+              head[i].className = 'navbar navbar-green';
+          }
+          for (var i = 0; i < headButton.length; i++) {
+              if (before === 'default') {
+                  headButton[i].classList.remove('btn-inverse');
+              }
+              headButton[i].classList.add('btn-default');
+          }
+          for (var i = 0; i < fa.length; i++) {
+              fa[i].setAttribute('style', 'color:white'); 
+          }
+          fp.setAttribute('style', 'background-color: #5CD65C'); 
+          for (var i = 0; i < p.length; i++) {
+              p[i].style.color = 'white'; 
+          }
+          for (var i = 0; i < h1.length; i++) {
+              h1[i].style.color = '#00990d'; 
+          }
+          diffp.style.color = 'white'; 
+          diffp.style.backgroundColor = 'white'; 
+          network.style.backgroundColor = '#EBFAEB'; 
+          footer.style.backgroundColor = '#ADEBAD'; 
+          footer.style.border = '#ADEBAD'; 
+          arp.style.backgroundColor = '#ADEBAD'; 
+          auth.style.backgroundColor = '#ADEBAD'; 
+          before = 'green';
+  } 
+  else if (color === 'default') {
     for (let i = 0; i < head.length; i++) {
       console.log(head[i]);
       head[i].className = 'navbar navbar-inverse';
